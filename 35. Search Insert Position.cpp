2e235906b5451a8f1fdf64df13a38e19a491cc7f
HunterMahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-04-04 17:09:03 +0600

    Create 35. Search Insert Position.cpp

---
+++ 35. Search Insert Position.cpp
@@ -0,0 +1,13 @@
+//url = https://leetcode.com/problems/search-insert-position
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int i=0;
+        for(i=0;i<nums.size(); i++){
+            if(nums[i]==target) return i;
+            if(nums[i]>target) return i;
+        }
+        return i;
+    }
+};
