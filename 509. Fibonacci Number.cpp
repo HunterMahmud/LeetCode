281241e54bba7f775b923fa55abf30f14fc81bf3
HunterMahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-04-04 18:11:34 +0600

    Create 509. Fibonacci Number.cpp

---
+++ 509. Fibonacci Number.cpp
@@ -0,0 +1,10 @@
+// url = https://leetcode.com/problems/fibonacci-number
+
+class Solution {
+public:
+    int fib(int n) {
+    if(n==0) return 0;
+    if(n==1) return 1;
+    else return fib(n-1)+fib(n-2);
+    }
+};
