3eabd38b09e847c518559871fed8ddea7c6bc680
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-06-04 18:42:57 +0600

    Update 1. Two Sum.cpp

--- 1. Two Sum.cpp
+++ 1. Two Sum.cpp
@@ -1,5 +1,22 @@
 //url = https://leetcode.com/problems/two-sum/
-
+//1st solution
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        int len = nums.size();
+        vector<int>ans;
+        for(int i=0;i<len-1;i++){
+            for(int j=i+1;j<len;j++){
+                if(nums[i]+nums[j]==target) {ans.push_back(i);
+                ans.push_back(j);}
+            }
+        }
+        return ans;
+    }
+        
+};
+///2nd solution
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
