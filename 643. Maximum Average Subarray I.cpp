165f3589a38c638d46e2010e74d3a2c55b80bd46
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-06-07 17:35:47 +0600

    Create 643. Maximum Average Subarray I.cpp

---
+++ 643. Maximum Average Subarray I.cpp
@@ -0,0 +1,18 @@
+// url = https://leetcode.com/problems/maximum-average-subarray-i
+
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        int n = nums.size();
+        //vector<int> res(nums.size());
+        for(int i=1;i<n;i++){
+            nums[i] = nums[i] +nums[i-1];
+        }
+        int mx = nums[k-1];
+        for(int i=k;i<n;i++){
+            if(mx < (nums[i]-nums[i-k])) mx = nums[i]-nums[i-k];
+        }
+        
+        return double(mx)/k;
+    }
+};
