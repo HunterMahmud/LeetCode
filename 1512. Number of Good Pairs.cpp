01a058b8045db08478ac8316d130ea5672815545
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-10-06 15:21:52 +0600

    Create 1512. Number of Good Pairs.cpp

---
+++ 1512. Number of Good Pairs.cpp
@@ -0,0 +1,14 @@
+// url = https://leetcode.com/problems/number-of-good-pairs
+
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        int cnt=0;
+        for(int i=0;i<nums.size()-1;i++){
+            for(int j=i+1; j< nums.size(); j++){
+                if(nums[i]==nums[j]) cnt++;
+            }
+        }
+        return cnt;
+    }
+};
