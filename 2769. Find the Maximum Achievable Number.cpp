dfb573cd4933f6523b376ca2f78c068098f23c63
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-10-09 15:33:58 +0600

    Create 2769. Find the Maximum Achievable Number.cpp

---
+++ 2769. Find the Maximum Achievable Number.cpp
@@ -0,0 +1,8 @@
+// url = https://leetcode.com/problems/find-the-maximum-achievable-number
+
+class Solution {
+public:
+    int theMaximumAchievableX(int num, int t) {
+        return t*2+num;
+    }
+};
