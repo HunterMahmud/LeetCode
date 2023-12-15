c78cb459f5a86a7012fef975730dde3a65893cff
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-12-15 22:43:28 +0600

    Create 1310. XOR Queries of a Subarray.cpp

---
+++ 1310. XOR Queries of a Subarray.cpp
@@ -0,0 +1,19 @@
+// url = https://leetcode.com/problems/xor-queries-of-a-subarray
+
+class Solution {
+public:
+    vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
+        vector<int> ans;
+        for(int i=1; i<arr.size(); i++){
+            arr[i] ^= arr[i-1];
+        }
+        for(int i=0; i<queries.size(); i++){
+            int x = queries[i][0];
+            int y = queries[i][1];
+           if(x==0) ans.push_back(arr[y]);
+           else ans.push_back(arr[x-1]^arr[y]);
+        }
+        return ans;
+    }
+};
+
