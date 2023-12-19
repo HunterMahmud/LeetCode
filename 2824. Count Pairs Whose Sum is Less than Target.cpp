dbda8026f3d0cb29ef0106f4fc4d7f27c6019908
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-12-19 07:02:41 +0600

    Update 2824. Count Pairs Whose Sum is Less than Target.cpp

--- 2824. Count Pairs Whose Sum is Less than Target.cpp
+++ 2824. Count Pairs Whose Sum is Less than Target.cpp
@@ -1,5 +1,6 @@
 // url = https://leetcode.com/problems/count-pairs-whose-sum-is-less-than-target
 
+// bruoteforce solution 
 
 class Solution {
 public:
@@ -12,6 +13,28 @@ public:
             }
         }
         return cnt;
+    }
+};
 
+
+// using two pointer solution
+
+class Solution {
+public:
+    int countPairs(vector<int>& nums, int target) {
+        int n = nums.size();
+        sort(nums.begin(),nums.end());
+        int cnt=0;
+        int i=0,j=n-1;
+        while(i<j){
+            if(nums[i]+nums[j]<target){
+                cnt += (j-i);
+                i++;
+            }
+            else{
+                j--;
+            }
+        }
+        return cnt;
     }
 };
