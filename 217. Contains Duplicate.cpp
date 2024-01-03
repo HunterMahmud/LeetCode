6a52c28d592b8fef28e581583868496de978fc56
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2024-01-03 07:43:43 +0600

    Create 217. Contains Duplicate.cpp

---
+++ 217. Contains Duplicate.cpp
@@ -0,0 +1,12 @@
+// url = https://leetcode.com/problems/contains-duplicate/
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        for(int i=1; i<nums.size(); i++){
+            if(nums[i]==nums[i-1]) return true;
+        }
+        return false;
+    }
+};
